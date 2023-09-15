d5f629dddf6a5f3436f5c9ad2971a33e384014b2
EmnaTaalouch <emna.taalouch@esprit.tn>
2023-09-15 16:03:55 +0100

    user

--- src/views/users/userList.js
+++ src/views/users/userList.js
@@ -1,69 +1,138 @@
 import React, { useState, useEffect } from 'react';
 import {
-  Typography,
-  Box,
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow,
-  IconButton,
-  Paper,
+    Typography,
+    Box,
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableRow,
+    IconButton,
+    Paper,
+    Button,
+    useTheme,
+    useMediaQuery,
+    Dialog,
+    DialogTitle,
+    DialogContent,
+    DialogContentText,
+    DialogActions,
+    InputLabel,
+    Select,
+    FormControl,
 } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
-
-// Sample user data
-const initialUsers = [
-  { id: 1, username: 'user1', email: 'user1@example.com' },
-  { id: 2, username: 'user2', email: 'user2@example.com' },
-  { id: 3, username: 'user3', email: 'user3@example.com' },
-];
+import { fetchUsers, removeUserFromList, updateUserFromList } from '../../redux/slices/userSlice';
+import { useDispatch, useSelector } from 'react-redux';
 
 const UserList = () => {
-  const [users, setUsers] = useState(initialUsers);
+    //const [users, setUsers] = useState(initialUsers);
+    const [selectedUserId, setSelectedUserId] = React.useState(null);
+    const [open, setOpen] = React.useState(false);
+    const theme = useTheme();
+    const users = useSelector((state) => state.user.users);
+    const handleClickOpen = (id) => {
+        setOpen(true);
+        setSelectedUserId(id); // Set the selected order ID here
+    };
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(fetchUsers());
+        console.log(users);
+    }, [dispatch]);
+    console.log(users);
 
-  // Function to delete a user
-  const deleteUser = (userId) => {
-    const updatedUsers = users.filter((user) => user.id !== userId);
-    setUsers(updatedUsers);
-  };
+    const handleClose = () => {
+        setOpen(false);
+    };
 
-  return (
-    <div>
-      <Typography variant="h4" gutterBottom>
-        Liste des clients
-      </Typography>
-      <Paper>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>ID</TableCell>
-              <TableCell>Nom du client</TableCell>
-              <TableCell>Email du client</TableCell>
-              <TableCell>Supprimer</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {users.map((user) => (
-              <TableRow key={user.id}>
-                <TableCell>{user.id}</TableCell>
-                <TableCell>{user.username}</TableCell>
-                <TableCell>{user.email}</TableCell>
-                <TableCell>
-                  <IconButton
-                    color="secondary"
-                    onClick={() => deleteUser(user.id)}
-                  >
-                    <DeleteIcon />
-                  </IconButton>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </Paper>
-    </div>
-  );
+    const handleDeleteUser = async (id) => {
+        console.log(id);
+
+        if (selectedUserId) {
+            dispatch(removeUserFromList(selectedUserId));
+            handleClose(); // Close the dialog
+        }
+    };
+    console.log(users);
+    return (
+        <div>
+            <Typography variant="h4" gutterBottom>
+                Liste des clients
+            </Typography>
+            <Paper>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>
+                                <Typography variant="subtitle2" fontWeight={600}>
+                                    ID
+                                </Typography>
+                            </TableCell>
+                            <TableCell>
+                                <Typography variant="subtitle2" fontWeight={600}>
+                                    Nom du client
+                                </Typography>
+                            </TableCell>
+                            <TableCell>
+                                <Typography variant="subtitle2" fontWeight={600}>
+                                    Email du client
+                                </Typography>
+                            </TableCell>
+                            <TableCell>Supprimer</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {users.map((user) => (
+                            <TableRow key={user.id}>
+                                <TableCell>
+                                    <Typography variant="subtitle2" fontWeight={600}>
+                                        {user.id}
+                                    </Typography>
+                                </TableCell>
+                                <TableCell>
+                                    <Typography variant="subtitle2" fontWeight={600}>
+                                        {user.first_name} {user.last_name}{' '}
+                                    </Typography>
+                                </TableCell>
+                                <TableCell>
+                                    <Typography variant="subtitle2" fontWeight={600}>
+                                        {user.email}
+                                    </Typography>
+                                </TableCell>
+                                <TableCell>
+                                    <IconButton
+                                        color="secondary"
+                                        onClick={() => handleClickOpen(user.id)}
+                                    >
+                                        <DeleteIcon />
+                                    </IconButton>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </Paper>
+
+            {/* Delete Confirmation Dialog */}
+            <Dialog open={open} onClose={handleClose} aria-labelledby="responsive-dialog-title">
+                <DialogTitle id="responsive-dialog-title">
+                    {"Suppression d'un utilisateur ! ! "}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText>Voulez-vous supprimer cet utilisateur?</DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button autoFocus onClick={handleClose}>
+                        annuler
+                    </Button>
+                    <Button onClick={() => handleDeleteUser(selectedUserId)} autoFocus>
+                        Supprimer
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
 };
 
 export default UserList;
